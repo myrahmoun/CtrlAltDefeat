21d2aeb2e07a9a44cd266fcc9724c1bcafeeebdb
Hannah Kim <hjkim@college.harvard.edu>
2025-12-12 16:16:55 -0500

    merged files

--- cards.py
+++ cards.py
@@ -104,6 +104,28 @@ class Hand:
 
     # def addActionCard(self, actionCard):
 
+'''
+Player Parameters:
+    - name: the name of the player
+    - role: 
+        - "adversary"
+        - "agent"
+    - hand: the hand of the player with 4 action cards and 2 objective cards
+    - boardPosition: the position of the player on the board, also their score. 
+    - playerStatus: the status of the player
+        - "waiting"
+        - "playing"
+        - "finished"
+'''
 
+class Player:
+    def __init__(self, name, role):
+        self.name = name
+        self.role = role
+        self.hand = cards.Hand()
+        self.boardPosition = 0
+        self.playerStatus = "waiting"
 
+        def __repr__(self):
+            return f"Player(name={self.name}, role={self.role}, hand={self.hand}, boardPosition={self.boardPosition}, status={self.status})"
 
