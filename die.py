334b51324cbe6d5668ab8c2c330def98544454be
MyRahmoun <myriamrmrm@gmail.com>
2026-07-28 20:06:45 -0400

    style

--- die.py
+++ die.py
@@ -1,18 +1,13 @@
-#!/usr/bin/evn python3
-# -*- coding: utf-8 -*-
 """
-Created on 12/12/25
-
-@author waldo
-
+Simple class to define and roll a die. 
 """
 import random
 
 class Die (object):
-    '''
+    """
     Class representing a die with a given number of sides. Currently the game only requires a six-sided die,
     but this class can be used for other types of dice as well.
-    '''
+    """
     def __init__(self, sides):
         self.sides = sides
 
